ccaf41bb3bfbfdadeb37ba632695eabbf041796e
daihp <489461197@qq.com>
2017-11-04 19:16:22 +0800

    首页Banner,楼层界面

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,17 +1,30 @@
 /*
 * @Author: Administrator
 * @Date:   2017-10-22 12:38:17
-* @Last Modified by:   Administrator
-* @Last Modified time: 2017-10-25 20:44:41
+* @Last Modified by:   daihp
+* @Last Modified time: 2017-11-04 19:15:11
 */
 
 'use strict';
-
+require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
+require('util/slider/index.js');
 var navSide = require('page/common/nav-side/index.js');
+var templateBanner = require('./banner.string');
 var _mm = require('util/mm.js');
 
-navSide.init({
-	name : 'order-list'
+$(function() {
+	// 渲染banner的html
+	var bannerHtml = _mm.renderHtml(templateBanner);
+	$('.banner-con').html(bannerHtml);
+   	// 初始化banner
+    var $slider    = $('.banner').unslider({
+    	dots: true
+    });
+    // 前一张和后一张的操作的事件绑定
+    $('.banner-con .banner-arrow').click(function(){
+    	var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+    	$slider.data('unslider')[forward]();
+    });
 });
---
+++ src/util/slider/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author: daihp
+* @Date:   2017-11-04 15:02:28
+* @Last Modified by:   daihp
+* @Last Modified time: 2017-11-04 15:15:36
+*/
+
+require('./index.css');
+require('./unslider.js');

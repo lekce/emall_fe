b28d8a1fc4ef4a06fa5bb8ce28036945ef1d54d1
daihp <489461197@qq.com>
2017-11-08 21:42:22 +0800

    商品列表页2

--- src/page/list/index.js
+++ src/page/list/index.js
@@ -2,7 +2,7 @@
 * @Author: daihp
 * @Date:   2017-10-28 15:09:45
 * @Last Modified by:   daihp
-* @Last Modified time: 2017-11-04 23:19:18
+* @Last Modified time: 2017-11-08 21:40:31
 */
 'use strict';
 require('./index.css');
@@ -30,19 +30,55 @@ var page = {
 		this.loadList();
 	},
 	bindEvent : function(){
-		
+		var _this = this;
+		_this.data.listParam.pageNum = 1;
+		// 排序点击事件
+		$('.sort-item').click(function(){
+			var $this = $(this);
+
+			if ($this.data('type') === 'default') {
+				if ($this.hasClass('active')) {
+					return;
+				}
+				else{
+					$this.addClass('active').siblings('.sort-item')
+						.removeClass('active asc desc');
+					_this.data.listParam.orderBy = 'default';
+				}
+			}
+			else if($this.data('type') === 'price'){
+				$this.addClass('active').siblings('.sort-item')
+						.removeClass('active asc desc');;
+				// 升序、降序的处理
+				if (!$this.hasClass('asc')) {
+					$this.addClass('asc').removeClass('desc');
+					_this.data.listParam.orderBy = 'price_asc';
+				}else{
+					$this.addClass('desc').removeClass('asc');
+					_this.data.listParam.orderBy = 'price_desc';
+				}
+			}
+			// 重新加载列表
+			_this.loadList();
+		});
 	},
 	// 加载list数据
 	loadList : function(){
 		var _this 		= this, 
 			listHtml 	= '',
-			listParam   = this.data.listParam;
-
+			listParam   = this.data.listParam,
+			$pListCon   = $('.p-list-con');
+		$pListCon.html('<div class="loading"></div>');
+		// 删除不必要的字段
+		listParam.categoryId ? 
+			(delete listParam.keyword) : 
+			(delete listParam.categoryId);
+		// 请求接口
 		_product.getProductList(listParam, function(res){
 			listHtml = _mm.renderHtml(templateIndex, {
 				list : res.list
 			});
-			$('.p-list-con').html(listHtml);
+			$pListCon.html(listHtml);
 			_this.loadPagination(res.pageNum, res.pages);
 		},function(errMsg){
 			_mm.errorTips(errMsg);

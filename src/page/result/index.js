188ee55e94aed5f801d36833faae9a9d80449915
daihp <489461197@qq.com>
2017-11-29 11:22:43 +0800

    支付模块

--- src/page/result/index.js
+++ src/page/result/index.js
@@ -1,16 +1,21 @@
 /*
 * @Author: Administrator
 * @Date:   2017-10-25 20:56:36
-* @Last Modified by:   Administrator
-* @Last Modified time: 2017-10-25 22:35:07
+* @Last Modified by:   daihp
+* @Last Modified time: 2017-11-29 11:20:17
 */
 require('./index.css');
 require('page/common/nav-simple/index.js');
 var _mm = require('util/mm.js');
 
 $(function(){
-	var type 		= _mm.getUrlParam('type') || 'default',
-		$element 	= $('.' + type + '-success');
-		// 显示对应的提示元素
-		$element.show();
+    var type        = _mm.getUrlParam('type') || 'default',
+        $element    = $('.' + type + '-success');
+    if(type === 'payment'){
+        var orderNumber  = _mm.getUrlParam('orderNumber'),
+            $orderNumber = $element.find('.order-number');
+        $orderNumber.attr('href', $orderNumber.attr('href') + orderNumber);
+    }
+    // 显示对应的提示元素
+    $element.show();
 })
